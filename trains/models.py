376d52ef544e30912a3899cca4d76575b37d05ec
Darman <n.darman05@gmail.com>
2022-11-24 10:11:58 +0600

    Метод модели clean

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,30 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+from cities.models import City
+
 # Create your models here.
+class Train(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Номер поезда')
+    travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути')
+    from_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='from_city_set', verbose_name='Из какога города')
+    to_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='to_city_set', verbose_name='В какой город')
+
+    def __str__(self):
+        return f'Поезд №{self.name} из города {self.from_city}'
+
+    def clean(self):
+        if self.from_city == self.to_city:
+            raise ValidationError('Изменить город прибытия')
+        qs = Train.objects.filter(from_city=self.from_city, to_city=self.to_city, travel_time=self.travel_time).exclude(pk=self.pk)
+        if qs.exists() :
+            raise ValidationError('Измените время в пути')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Поезд'
+        verbose_name_plural = 'Поезда'
+        ordering = ['name']
